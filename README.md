3343cd9630d6764a034d12cae7a5382a0039aafe
Georgie <hello@georgie.nu>
2015-10-10 10:08:07 +0700

    Complete build

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # g
-Build for georgieluhur.com
+
+Build for my personal portfolio, [georgieluhur.com](http://georgieluhur.com). Launched 10 Oct 2015, built to update the old design and maintain consistent branding with [Hey Georgie](http://hey.georgie.nu). At present I would still consider it MVP. ;)
+
+The CSS is written in [Sass](http://sass-lang.com) with [BEM](http://bem.info) naming convention, and following a modular file structure based on [SMACSS](http://smacss.com).
+
+Browser support is IE9+. Still viewable in IE8, just not optimised.
+
+Email **g @ my first name dot nu** if you have any suggestions for improvement.
---
+++ index.php
@@ -0,0 +1,267 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Georgie Luhur</title>
+        <meta charset="utf-8">
+        <meta name="description" content="Georgie Luhur – front-end web developer and concert photographer based in Sydney, Australia.">
+        <meta name="viewport" content="initial-scale=1.0, width=device-width">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header class="site-header">
+            <nav class="site-navigation" role="navigation">
+                <a class="site-navigation__link--home" href="/">Georgie Luhur</a>
+                <a class="site-navigation__link" href="http://hey.georgie.nu/">Blog</a>
+            </nav>
+        </header>
+        <section class="intro">
+            <div class="site-section-inner">
+                <div class="intro__headline">
+                    <img class="intro__photo" alt="I’m Georgie! This is a photo of me." src="images/Georgie.jpg" width="200" height="200">
+                    <p>Hey, I’m Georgie. I’m a <strong>front-end web developer</strong>, concert photographer and <a href="http://hey.georgie.nu/">blogger</a>.</p>
+                    <p>I have a passion for CSS, semantics, and accessibility, and I believe microdata has a future. I appreciate well-written documentation, and unlike most other developers, I love building emails.</p>
+                </div>
+                <ul class="contact-detail">
+                    <li class="contact-detail__block">
+                        <a href="mailto:hello@georgie.nu">Email me</a>
+                    </li>
+                    <li class="contact-detail__block">
+                        <a href="http://twitter.com/georgiecel/">Twitter</a>
+                    </li>
+                    <li class="contact-detail__block">
+                        <a href="http://linkedin.com/in/gluhur/">LinkedIn</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="site-section">
+            <div class="site-section-inner">
+                <h2>Work experience</h2>
+                <ul class="list">
+                    <li>
+                        <h3>hipages Group</h3>
+                        <span class="list__subheading">Front End Engineer</span>
+                        <span class="list__date">June – October 2015</span>
+                        <ul class="list__details">
+                            <li><em>Natural Therapy Pages</em> and <em>Petpages</em> – worked with a monolithic, five-year-old codebase to make improvements to SEO, rebuilt header and footer components to follow a BEM naming convention, and improved mobile friendliness of website</li>
+                            <li><em>Home Improvement Pages</em> – worked on crucial feature requests to help increase revenue in the Job Monetisation team</li>
+                            <li><em>Project Autobot</em> – rebuilt old email and print templates for the HIP invoicing system, conducted printing tests and various other tests to consider all edge cases</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>Freelancer Limited</h3>
+                        <span class="list__subheading">Front End Developer</span>
+                        <span class="list__date">February 2014 – June 2015</span>
+                        <ul class="list__details">
+                            <li><em>Skills Lab</em> – rebranding and rebuild of Freelancer.com’s Exam section, built to deliver a new and more enjoyable experience to the users (won first prize in a company hackathon)</li>
+                            <li><em>Warrior Payments</em> – an all-in-one payments, analytics, affiliate marketing and distribution platform, built from scratch with a team of engineers</li>
+                            <li><em>Warrior Forum</em> – improved the UI of Warrior Forum and built sub-products of the Warrior brand, including the beta version of <em>Warrior Experts</em></li>
+                            <li><em>POTUS/Acquisition</em> – built new landing pages and responsive emails, set up AB tests and organised email campaigns</li>
+                            <li><em>Freelancer.com</em> – replaced all buttons in the codebase with more up-to-date CSS, worked closely with designers to develop new features and improve consistency of branding</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>The Working Party</h3>
+                        <span class="list__subheading">Front End Web Developer</span>
+                        <span class="list__date">November 2011 – November 2013</span>
+                        <ul class="list__details">
+                            <li><em>transportnsw.info</em> – built the New South Wales transport website from scratch, using Jekyll and Stylus with SMACSS methodology</li>
+                            <li>Built and maintained e-commerce websites with Shopify</li>
+                            <li>Built websites in MODX, Textpattern and WordPress</li>
+                            <li>Built email templates and sent campaigns using Campaign Monitor and MailChimp</li>
+                            <li>Attended meetings with clients to build product roadmaps and wireframes, conduct content audits and improve information architecture, liaised via phone/email to perform website maintenance</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>Kumon Education Centre</h3>
+                        <span class="list__subheading">Student Marker/Assistant</span>
+                        <span class="list__date">February 2008 – February 2012</span>
+                        <ul class="list__details">
+                            <li>Marked the work of students participating in the Kumon program for English and Mathematics</li>
+                            <li>Personally assisted students with their work, including high school students and students with learning difficulties</li>
+                            <li>Performed administration duties such as writing receipts, minding the front desk and packing up the centre</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>Ziller Web Design Sydney</h3>
+                        <span class="list__subheading">Front End Web Developer</span>
+                        <span class="list__date">September – November 2011</span>
+                        <ul class="list__details">
+                            <li>Utilised an in-house CMS to update the websites of various clients</li>
+                            <li>Built and updated email templates, and sent campaigns using MailChimp</li>
+                            <li>Converted PSDs to HTML pages</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>Cambridge University Press</h3>
+                        <span class="list__subheading">Graphic Designer</span>
+                        <span class="list__date">November 2009 – February 2010</span>
+                        <ul class="list__details">
+                            <li><em>Maths-in-a-Box</em> – built screens of the computer application based on an existing physical flash card kit</li>
+                            <li>Basic image editing in Photoshop, working with an existing system to upload and input images and data</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="site-section">
+            <div class="site-section-inner">
+                <h2>Side projects</h2>
+                <ul class="list">
+                    <li>
+                        <h3><a href="http://hey.georgie.nu/">Hey Georgie</a></h3>
+                        <span class="list__subheading">Design, development and blogging</span>
+                        <span class="list__date">2007 – present</span>
+                        <p>Blogging is something I am very passionate about, and I write all content for my blog (with the exception of a couple of interviews and a quarterly fashion post). I designed the layout for my blog and also wrote all the code. It’s like my playground where I show off a lot of bits and pieces that I learn, as well as write about other things I am interested in.</p>
+                    </li>
+                    <li>
+                        <h3><a href="http://nicholascooke.com/">Nicholas Cooke</a></h3>
+                        <span class="list__subheading">Design, development and maintenance</span>
+                        <span class="list__date">February 2015 – present</span>
+                        <p>This is a one-page website for Nicholas Cooke, a product manager and data scientist from Sydney. He needed a quick, simple and clean look. At present, there are further plans to update the website.</p>
+                    </li>
+                    <li>
+                        <h3><a href="http://alonelyseptember.org/">A Lonely September</a></h3>
+                        <span class="list__subheading">Design and development</span>
+                        <span class="list__date">December 2013</span>
+                        <p>I created this website design for Sydney university graduate Seb, who is a passionate writer of various topics including music, anime, games, books and photography. He was after a clean and simple responsive layout.</p>
+                    </li>
+                    <li>
+                        <h3><a href="http://caity.nu/">caity.nu</a></h3>
+                        <span class="list__subheading">Design and development</span>
+                        <span class="list__date">November 2013</span>
+                        <p>Caity is a blogger from New Zealand, originally from the United States. She was after a new theme without red and yellow, but with some colour. This design was created to suit her bubbly and optimistic personality.</p>
+                    </li>
+                    <li>
+                        <h3><a href="http://6birds.net/">6birds.net</a></h3>
+                        <span class="list__subheading">Design and development</span>
+                        <span class="list__date">September – November 2013</span>
+                        <p>I created a responsive WordPress theme for Liz’s personal blog. She wanted something different from the previous blue design I had created for her. She supplied a link to a <a href="http://www.youtube.com/watch?v=Jwr9JwyOHqA">music video clip</a> for a song that she liked, and invited me to take inspiration from the style of the video to design something “calm and subtle”.</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="site-section">
+            <div class="site-section-inner">
+                <h2>Education</h2>
+                <ul class="list">
+                    <li>
+                        <h3>Master of Interactive Multimedia</h3>
+                        <span class="list__subheading">University of Technology, Sydney</span>
+                        <span class="list__date">2012 – 2013</span>
+                        <ul class="list__details">
+                            <li><em>Alzheimer’s Hub</em> – worked in a team to build a product from the ground up, including all research and planning, for a resource aimed at carers of Alzheimer sufferers</li>
+                            <li><em>Opal app</em> – worked in a team to develop a detailed scope and product plan for a phone application designed for use with the Opal transport card</li>
+                            <li>Completed a number of interaction design projects involving written work as well as graphic and animation design</li>
+                            <li>Sharpened skills in HTML5, PHP and responsive web design in a self-driven project</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>Responsive Web Design and SASS</h3>
+                        <span class="list__subheading">Max Design</span>
+                        <span class="list__date">2013</span>
+                        <ul class="list__details">
+                            <li>Learned about basics and best practices of responsive web design, with and without a framework or boilerplate</li>
+                            <li>Covered various tips and solutions for mobile-friendly navigation, images, tables and text</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>User Experience (UX) Design</h3>
+                        <span class="list__subheading">Peak Usability</span>
+                        <span class="list__date">2012</span>
+                        <ul class="list__details">
+                            <li>Learned about UX, implementing UX tools and methodologies in the workplace, and how to solve real-world problems</li>
+                            <li>Workshopped various ideas and problem solving both individually and in small groups</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>Bachelor of Arts in Communication (Information and Media)</h3>
+                        <span class="list__subheading">University of Technology, Sydney</span>
+                        <span class="list__date">2009 – 2011</span>
+                        <ul class="list__details">
+                            <li><em>Media sub-major</em> – I undertook my own user research after developing my own theses related to personal blogging</li>
+                            <li><em>Photography</em> – I minored in photography, where I learned about traditional darkroom processes and modern studio lighting</li>
+                            <li>Completed a number of interaction design projects involving written work as well as graphic and animation design</li>
+                            <li>Familiarised myself with database management and data mining processes</li>
+                            <li>Sharpened skills in HTML5, PHP and responsive web design in a self-driven project</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="site-section">
+            <div class="site-section-inner">
+                <h2>Social profiles</h2>
+                <ul class="list--columns">
+                    <li class="list__block">
+                        <h3>GitHub: <a href="http://github.com/georgiecel/">georgiecel</a></h3>
+                    </li>
+                    <li class="list__block">
+                        <h3>Instagram: <a href="http://instagram.com/hey.georgie/">hey.georgie</a></h3>
+                    </li>
+                    <li class="list__block">
+                        <h3>LinkedIn: <a href="http://linkedin.com/in/gluhur/">/gluhur</a></h3>
+                    </li>
+                    <li class="list__block">
+                        <h3>Twitter: <a href="http://twitter.com/georgiecel/">@georgiecel</a></h3>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="site-section">
+            <div class="site-section-inner">
+                <h2>Volunteering</h2>
+                <ul class="list">
+                    <li>
+                        <h3><a href="http://casualbandblogger.com/">Casual Band Blogger</a></h3>
+                        <span class="list__subheading">Photographer &amp; Music Reviewer</span>
+                        <span class="list__date">July 2013 – present</span>
+                        <ul class="list__details">
+                            <li>Photograph concerts as needed, with the responsibility of uploading photos to the website.</li>
+                            <li>Regularly write reviews for new music, such as singles, albums and music videos.</li>
+                            <li>Write reviews for concerts if required.</li>
+                            <li>Transcribe audio interviews or conduct interviews with musicians, either by phone or email.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3><a href="http://thefanlistings.org/">The Fanlistings Network</a></h3>
+                        <span class="list__subheading">Category Staffer, Trouble Checker</span>
+                        <span class="list__date">November 2012 – present</span>
+                        <ul class="list__details">
+                            <li><em>Songs: Bands/Groups 0-M</em>, <em>Songs: Bands/Groups N-Z</em> and <em>Websites</em> – processing application forms, updated information forms, closed forms, and conducting trouble checks, then writing update posts and conducting regular discussion in the message boards</li>
+                            <li><em>Songs: Various</em> and <em>Musicians: Bands/Groups</em> – performing a check every two months to ensure that all listed websites are following rules and meeting the network’s criteria</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3><a href="http://tonedeaf.com.au/">Tone Deaf</a></h3>
+                        <span class="list__subheading">Photographer</span>
+                        <span class="list__date">June 2013 – January 2015</span>
+                        <ul class="list__details">
+                            <li>Photograph concerts as needed, with the responsibility of uploading photos to the website.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3><a href="http://amusicblogyea.com/">A Music Blog, Yea?</a></h3>
+                        <span class="list__subheading">Photographer &amp; Music Reviewer</span>
+                        <span class="list__date">January 2013 – January 2014</span>
+                        <ul class="list__details">
+                            <li>Photograph concerts and send photos to content co-ordinators.</li>
+                            <li>Review new Australian music and concerts to add more diverse content to the website, as it was based in Canada.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3><a href="https://headon.com.au">Head On Photo Festival</a></h3>
+                        <span class="list__subheading">Photo Gallery Supervisor</span>
+                        <span class="list__date">June 2012</span>
+                        <ul class="list__details">
+                            <li>Supervised the Burn photo gallery at Bondi, assisted in counting visitor numbers, minding the front desk, handing out flyers and packing the gallery at the end of the exhibition</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <footer class="site-footer">
+            Footers are overrated. :3 &nbsp;|&nbsp; This design is MVP <em>#noshame</em> &nbsp;|&nbsp; <a href="mailto:hello@georgie.nu">email me though.</a>
+        </footer>
+    </body>
+</html>
