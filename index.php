35335d751f5d1493efd18b4dae893ec0ad080cde
Georgie <hello@georgie.nu>
2015-10-17 12:21:19 +0700

    Adding GA tracking

--- index.php
+++ index.php
@@ -263,5 +263,13 @@
         <footer class="site-footer">
             Footers are overrated. :3 &nbsp;|&nbsp; This design is MVP <em>#noshame</em> &nbsp;|&nbsp; <a href="mailto:hello@georgie.nu">email me though.</a>
         </footer>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-64300477-2', 'auto');
+            ga('send', 'pageview');
+        </script>
     </body>
 </html>
